0f08609ec44af216cfad6c23699830448a7d1279
L.B <mrlawrencelane@gmail.com>
2022-09-22 19:30:15 -0400

    wideless fix?

--- themes/pach-emdash/assets/js/algoliaHomePage.js
+++ themes/pach-emdash/assets/js/algoliaHomePage.js
@@ -34,7 +34,7 @@ const searchClient = algoliasearch('RUV2F528SR', '1f21e218181a4f87c5496cd574a88c
         form: ['spread'],
         submit: ['is-hidden'],
         reset: ['is-hidden'],
-        input: ['is-full','white', 'rounded-4', 'p-1']
+        input: ['is-full','darken-1', 'rounded-4', 'p-1']
       },
       templates: {
       },
