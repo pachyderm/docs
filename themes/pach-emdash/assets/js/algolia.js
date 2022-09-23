3b42dcf96f85cc9942d17503e3c1fbf1c58d37c1
L.B <mrlawrencelane@gmail.com>
2022-09-23 08:45:44 -0400

    rem and vmin

--- themes/pach-emdash/assets/js/algolia.js
+++ themes/pach-emdash/assets/js/algolia.js
@@ -27,7 +27,7 @@ const searchClient = algoliasearch('RUV2F528SR', '1f21e218181a4f87c5496cd574a88c
         form: ['spread'],
         submit: ['is-hidden'],
         reset: ['is-hidden'],
-        input: ['is-full','white', 'rounded-4', 'sp-1']
+        input: ['is-full','darken-1', 'rounded-4', 'sp-2']
       },
       templates: {
       },
@@ -41,24 +41,24 @@ const searchClient = algoliasearch('RUV2F528SR', '1f21e218181a4f87c5496cd574a88c
       escapeHTML: false,
       cssClasses: {
         root: ['spread-center','modal', 'darken-5'],
-        list: ['is-three-fifths','white', 'pr-1', 'rounded-1', 'brighten-1', 'is-fullsize-mobile'],
+        list: ['is-three-fifths','white', 'pr-2', 'rounded-1', 'brighten-1', 'is-fullsize-mobile'],
         item: ['spread'] },
       templates: {
-        empty: `<div class="hit spread mt-5 pinned-top is-full darken-1 rounded-1 c-sp-1 m-1"> <div class="white text-center rounded-1"><h2 class="uppercase bold">No Results Found</h2>
+        empty: `<div class="hit spread mt-5 pinned-top is-full darken-1 rounded-1 c-sp-2 m-2"> <div class="white text-center rounded-1"><h2 class="uppercase bold">No Results Found</h2>
         <div class="subtitle-1"> Could not locate results matching <strong>{{query}}</strong>. </div> `,
         item: `
           <div class="stack">
-            <a href="{{relURI}}" class="sp-1 outlined rounded-1 move-l">
-              <div class="hit spread c-pinned-center is-full c-mr-1">
-                <div class="black gradient-lr-cold is-fit xxs rounded-1 darken-3 p-1">{{version}}</div>
-                <div class="stack c-mb-1">
-                  <p class="xxs uppercase is-fit">{{parent}}</p>
+            <a href="{{relURI}}" class="sp-2 outlined rounded-1 move-l">
+              <div class="hit spread c-pinned-center is-full c-mr-2">
+                <div class="black gradient-lr-cold is-fit xxs rounded-1 darken-3 p-2">{{version}}</div>
+                <div class="stack c-mb-2">
+                  <div class="xxs uppercase is-fit">{{parent}}</div>
                   <h3 class="is-fit s extra-bold">{{title}}</h3>
                   <div class="xs">{{description}}</div>
                 </div>
               </div>
             </a>
-            <div class="xxs spread-right c-black c-mb-1 c-mr-1 c-fit">{{#tags}} <a href="/tags/{{.}}?&v={{version}}">{{.}}</a>{{/tags}}</div>
+            <div class="xxs spread-right c-black c-mb-2 c-mr-2 c-fit">{{#tags}} <a href="/tags/{{.}}?&v={{version}}">{{.}}</a>{{/tags}}</div>
           </div>
           `
       },
--- themes/pach-emdash/assets/js/algoliaHomePage.js
+++ themes/pach-emdash/assets/js/algoliaHomePage.js
@@ -34,7 +34,7 @@ const searchClient = algoliasearch('RUV2F528SR', '1f21e218181a4f87c5496cd574a88c
         form: ['spread'],
         submit: ['is-hidden'],
         reset: ['is-hidden'],
-        input: ['is-full','darken-1', 'rounded-4', 'p-1']
+        input: ['is-full','darken-1', 'rounded-4', 'p-2']
       },
       templates: {
       },
@@ -51,22 +51,22 @@ const searchClient = algoliasearch('RUV2F528SR', '1f21e218181a4f87c5496cd574a88c
         list: ['spread-left', 'rounded-1', 'is-fullsize-mobile'],
         item: ['is-half', 'is-fullsize-mobile'] },
       templates: {
-        empty: `<div class="hit spread mt-5 pinned-top is-full brighten-1 rounded-1 c-sp-1 m-1"> <div class=" text-center rounded-1"><h2 class="uppercase bold">No Results Found</h2>
+        empty: `<div class="hit spread mt-5 pinned-top is-full brighten-1 rounded-1 c-sp-2 m-2"> <div class=" text-center rounded-1"><h2 class="uppercase bold">No Results Found</h2>
         <div class="subtitle-1"> Could not locate results matching <strong>{{query}}</strong>. </div> `,
         item: `
-          <div class="stack outlined rounded-1 move-l sp-1">
+          <div class="stack outlined rounded-1 move-l sp-2">
             <a href="{{relURI}}">
-              <div class="hit spread c-pinned-center is-full c-mr-1">
-                <div class="black gradient-lr-cold is-fit xxs rounded-1 darken-3 p-1">{{version}}</div>
-                <div class="stack c-mb-1">
-                  <p class="xs uppercase is-fit">{{parent}}</p>
+              <div class="hit spread c-pinned-center is-full c-mr-2">
+                <div class="black gradient-lr-cold is-fit xxs rounded-1 darken-3 p-2">{{version}}</div>
+                <div class="stack c-mb-2">
+                  <div class="xs uppercase is-fit">{{parent}}</div>
                   <h3 class="is-fit m extra-bold">{{title}}</h3>
                   <div class="xs">{{description}}</div>
                 </div>
               </div>
             </a>
           </div>
-          <div class="xs spread-right c-black c-mb-2 c-mr-1 c-fit">{{#tags}} <a href="/tags/{{.}}?&v={{version}}">{{.}}</a>{{/tags}}</div>
+          <div class="xs spread-right c-black c-mb-3 c-mr-2 c-fit">{{#tags}} <a href="/tags/{{.}}?&v={{version}}">{{.}}</a>{{/tags}}</div>
           `
       },
       transformData: {
