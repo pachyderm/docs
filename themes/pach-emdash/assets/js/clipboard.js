950d59dbc26c347010985f35cd4547411768c57f
L.B <mrlawrencelane@gmail.com>
2022-09-27 12:15:07 -0400

    fixed clipboard button

--- themes/pach-emdash/assets/js/clipboard.js
+++ themes/pach-emdash/assets/js/clipboard.js
@@ -3,22 +3,16 @@
         var element = elements[i];
 
         if (element.innerText.length > 0) {
-         
-            var copyButton = document.createElement('button');
-            copyButton.classList.add( 'clippy', 'p-1', 'darken-1', 'rounded-3', 'uppercase', 'xxs', 'is-fit', 'pinned-end');
 
-            copyButton.innerHTML = 'Copy';
-            element.appendChild(copyButton);
-
-            element.addEventListener('click', function(e) {
+            // listen to the grandparent element for clicks
+            element.parentElement.parentElement.addEventListener('click', function(e) {
                 var code = this.getElementsByTagName('code')[0];
                 navigator.clipboard.writeText(code.innerText)
-                // update the button text
-                this.getElementsByClassName('clippy')[0].innerHTML = 'Copied!';
+                this.parentElement.parentElement.getElementsByClassName('clippy')[0].innerHTML = 'Copied!';
                 // reset the button text after a few seconds
                 setTimeout(function() {
-                    this.getElementsByClassName('clippy')[0].innerHTML = 'Copy';
+                    this.parentElement.parentElement.getElementsByClassName('clippy')[0].innerHTML = 'Copy';
                 }.bind(this), 2000);
-            }.bind(element));
+            })
         }
-    }
+}
