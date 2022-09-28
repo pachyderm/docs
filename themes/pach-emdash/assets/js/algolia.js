8893497efc53bc9b52378a0b726bdc7fc7f57e6f
L.B <mrlawrencelane@gmail.com>
2022-09-28 12:08:24 -0400

    search padding

--- themes/pach-emdash/assets/js/algolia.js
+++ themes/pach-emdash/assets/js/algolia.js
@@ -40,15 +40,15 @@ const searchClient = algoliasearch('RUV2F528SR', '1f21e218181a4f87c5496cd574a88c
       container: '#hits',
       escapeHTML: false,
       cssClasses: {
-        root: ['spread-center','modal', 'darken-5', 'mt-2'],
+        root: ['spread-center','modal', 'darken-5', 'mt-3'],
         list: ['is-three-fifths','white', 'pr-2', 'rounded-1', 'brighten-1', 'is-fullsize-mobile'],
         item: ['spread'] },
       templates: {
         empty: `<div class="hit spread mt-5 pinned-top is-full darken-1 rounded-1 c-sp-2 m-2"> <div class="white text-center rounded-1"><h2 class="uppercase bold">No Results Found</h2>
         <div class="subtitle-1"> Could not locate results matching <strong>{{query}}</strong>. </div> `,
         item: `
-          <div class="stack outlined rounded-4 move-l m-2 c-m-3">
-            <a href="{{relURI}}" class="">
+          <div class="stack outlined rounded-4 move-l m-2 p-1">
+            <a href="{{relURI}}" class="m-1 c-m-1">
               <div class="hit spread c-pinned-center is-full ">
                 <div class="stack c-mb-2">
                   <div class="xxs uppercase is-fit">{{parent}}</div>
@@ -57,7 +57,7 @@ const searchClient = algoliasearch('RUV2F528SR', '1f21e218181a4f87c5496cd574a88c
                 </div>
               </div>
             </a>
-            <div class="xs spread-right c-black c-mr-2 c-fit">{{#tags}} <a href="/tags/{{.}}?&v={{version}}">{{.}}</a>{{/tags}}</div>
+            <div class="xs spread-right c-black m-1 c-mr-2 c-fit">{{#tags}} <a href="/tags/{{.}}?&v={{version}}">{{.}}</a>{{/tags}}</div>
           </div>
           `
       },
