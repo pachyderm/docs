1f7bc0159c11832a8171239e141e10835f6c9acb
L.B <mrlawrencelane@gmail.com>
2022-10-17 11:31:35 -0400

    click to swipe

--- themes/pach-emdash/assets/js/relatedArticles.js
+++ themes/pach-emdash/assets/js/relatedArticles.js
@@ -1,1 +1,10 @@
- 
+ // when the button with id "see-related" is clicked, scroll to the element with the id "related-articles" without using jQuery
+
+
+var seeRelated = document.getElementById('see-related');
+var relatedArticles = document.getElementById('related-articles');
+
+seeRelated.addEventListener('click', function() {
+    relatedArticles.scrollIntoView({behavior: "smooth"});
+});
+
