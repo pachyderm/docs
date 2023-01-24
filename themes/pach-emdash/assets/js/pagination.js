64d07f603a647202b6a0b309b8b07eaffde36633
L.B <mrlawrencelane@gmail.com>
2023-01-24 11:27:12 -0500

    paginator improvement

--- themes/pach-emdash/assets/js/pagination.js
+++ themes/pach-emdash/assets/js/pagination.js
@@ -10,6 +10,28 @@ currentPager = paginator.children[0]
       lastVisiblePager.classList.toggle('is-hidden');
       currentPager = document.getElementById(pager)
       currentPager.classList.toggle('is-hidden');
+      // remove black button class from last visible pager
+   
    }
+
+
+   // get all the button elements
+   const buttons = document.querySelectorAll('button');
+   console.log (buttons)
+   // if the button is clicked, toggle the black class
+   let lastButtonClicked = null;
+   buttons.forEach(button => {
+      button.addEventListener('click', function() {
+         button.classList.toggle('black');
+         button.classList.toggle("darken-7");
+         button.classList.toggle("brighten-7");
+         if (lastButtonClicked) {
+            lastButtonClicked.classList.toggle('black');
+            lastButtonClicked.classList.toggle("darken-7");
+            lastButtonClicked.classList.toggle("brighten-7");
+         }
+         lastButtonClicked = button;
+      });
+   } );
 }
 
