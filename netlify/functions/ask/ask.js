b2ed304548b319f810c6b2a0cc759f740d8aaebf
L.B <mrlawrencelane@gmail.com>
2023-03-15 15:42:46 -0400

    js changes

---
+++ netlify/functions/ask/ask.js
@@ -0,0 +1,22 @@
+import('chatgpt').then(({ ChatGPTAPI }) => {
+  const handler = async (event) => {
+    try {
+      const question = event.queryStringParameters.question || 'Hello World!'
+      const api = new ChatGPTAPI({
+        apiKey: process.env.OPENAI_API_KEY
+      })
+      const res = await api.sendMessage(question, {
+        // print the partial response as the AI is "typing"
+        onProgress: (partialResponse) => console.log(partialResponse.text)
+      })
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ message: res.text }),
+      }
+    } catch (error) {
+      return { statusCode: 500, body: error.toString() }
+    }
+  }
+  
+  module.exports.handler = handler
+})
