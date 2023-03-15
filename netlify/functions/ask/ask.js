765795dbe4ea30a9a15f66d6c37c70556db98c81
L.B <mrlawrencelane@gmail.com>
2023-03-15 15:15:32 -0400

    test

--- netlify/functions/ask/ask.js
+++ netlify/functions/ask/ask.js
@@ -1,25 +1,24 @@
-// This function takes a question from the /ask endpoint and returns an answer from the ChatGPT API.
+
 
 import { ChatGPTAPI } from 'chatgpt'
 
 const handler = async (event) => {
-    try {
-        console.log("event: ", event)
-        const question = event.queryStringParameters.question || 'Hello World!'
-        const api = new ChatGPTAPI({
-            apiKey: process.env.OPENAI_API_KEY
-        })
-        const res = await api.sendMessage(question, {
-            // print the partial response as the AI is "typing"
-            onProgress: (partialResponse) => console.log(partialResponse.text)
-          })
-        return {
-            statusCode: 200,
-            body: JSON.stringify({ message: res.text }),
-        }
-    } catch (error) {
-        return { statusCode: 500, body: error.toString() }
+  try {
+    const question = event.queryStringParameters.question || 'Hello World!'
+    const api = new ChatGPTAPI({
+      apiKey: process.env.OPENAI_API_KEY
+    })
+    const res = await api.sendMessage(question, {
+      // print the partial response as the AI is "typing"
+      onProgress: (partialResponse) => console.log(partialResponse.text)
+    })
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: res.text }),
     }
+  } catch (error) {
+    return { statusCode: 500, body: error.toString() }
   }
-  
-  module.exports = { handler }
+}
+
+export { handler }
