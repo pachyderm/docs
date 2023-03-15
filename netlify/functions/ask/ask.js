9edb89f047916d784c43daaaf5a386cd3a9d81d1
L.B <mrlawrencelane@gmail.com>
2023-03-15 18:48:31 -0400

    test

---
+++ netlify/functions/ask/ask.js
@@ -0,0 +1,31 @@
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+
+async function handler(event) {
+    try {
+        const subject = event.queryStringParameters.name || 'Hello World'
+        console.log("subject", subject)
+
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: subject,
+            temperature: 0,
+            max_tokens: 7,
+        });
+
+        console.log("response", response)
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: response }),
+        }
+    } catch (error) {
+        return { statusCode: 500, body: error.toString() }
+    } 
+}
+
+module.exports = { handler }
