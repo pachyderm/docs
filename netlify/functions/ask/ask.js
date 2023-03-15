484e12b7b9c971c11a31f1cfe506e7b9b311ebcd
L.B <mrlawrencelane@gmail.com>
2023-03-15 19:02:40 -0400

    update

--- netlify/functions/ask/ask.js
+++ netlify/functions/ask/ask.js
@@ -4,17 +4,38 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
+async function getEmbeddings(texts) {
+    const embeddings = [];
+
+    for (let i = 0; i < texts.length; i++) {
+        const response = await openai.embed(texts[i], "text-embedding-2020-09-01");
+        embeddings.push(response.data.embeddings[0]);
+    }
+
+    return embeddings;
+}
+
+async function answerQuestion(question, embeddings) {
+    const response = await openai.search(
+        question,
+        embeddings,
+        "text-embedding-2020-09-01"
+    );
+
+    return response.data.results[0].text;
+}
+
 
 async function handler(event) {
     try {
-        const subject = event.queryStringParameters.name || 'Hello World'
+        const subject = event.queryStringParameters.question || 'What is Pachyderm?'
         console.log("subject", subject)
 
         const response = await openai.createCompletion({
             model: "text-davinci-003",
             prompt: subject,
             temperature: 0,
-            max_tokens: 7,
+            max_tokens: 200,
         });
 
         console.log("response", response)
