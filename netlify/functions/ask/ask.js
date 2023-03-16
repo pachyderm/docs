7eb4916e4c88b68e716646f0835a7bd5b01fb9c3
L.B <mrlawrencelane@gmail.com>
2023-03-16 10:51:14 -0400

    createanswerrequest

--- netlify/functions/ask/ask.js
+++ netlify/functions/ask/ask.js
@@ -1,4 +1,4 @@
-const { Configuration, OpenAIApi } = require("openai");
+const { Configuration, OpenAIApi, CreateAnswerRequest } = require("openai");
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
@@ -8,61 +8,68 @@ const Papa = require("papaparse");
 const axios = require("axios");
 
 async function getEmbeddings() {
-    const response = await axios.get("https://deploy-preview-39--pach-docs.netlify.app/embeddings/embeddings.csv");
-    const embeddings = Papa.parse(response.data, {
-      header: true,
-      dynamicTyping: true,
-      skipEmptyLines: true,
-    }).data;
-    return embeddings;
-  }
-  
+  const response = await axios.get("https://deploy-preview-39--pach-docs.netlify.app/embeddings/embeddings.csv");
+  const embeddings = Papa.parse(response.data, {
+    header: true,
+    dynamicTyping: true,
+    skipEmptyLines: true,
+  }).data;
+  return embeddings;
+}
 
 // function to calculate cosine similarity between two vectors
 function cosineSimilarity(a, b) {
-    const dotProduct = a.reduce((acc, val, i) => acc + val * b[i], 0);
-    const normA = Math.sqrt(a.reduce((acc, val) => acc + val * val, 0));
-    const normB = Math.sqrt(b.reduce((acc, val) => acc + val * val, 0));
-    return dotProduct / (normA * normB);
-  }
-
-  async function handler(event) {
-    try {
-        const embeddings = await getEmbeddings();
-
-        const userQuestion = event.queryStringParameters.question || 'What is Pachw?';
-        console.log("subject", userQuestion);
-
-        const similarities = embeddings.map((embedding) => {
-            const article = embedding.text;
-            const embeddingVector = embedding.embeddings.split(',').map(parseFloat);
-            const userQuestionVector = userQuestion.split(' ').map((word) => embeddingVector[embedding.text.indexOf(word)] || 0);
-            const similarity = cosineSimilarity(embeddingVector, userQuestionVector);
-            return { article, similarity };
-        });
-        
-        // Sort the similarities in descending order by the similarity score
-        similarities.sort((a, b) => b.similarity - a.similarity);
-
-        const searchResponse = await openai.search({
-            documents: similarities.map((similarity) => similarity.article),
-            query: userQuestion,
-            max_rerank: 200,
-        });
-
-        console.log("searchResponse", searchResponse);
-
-        const answer = searchResponse.data.length > 0 ? searchResponse.data[0].text : "Sorry, I don't know the answer.";
-        console.log("answer", answer);
-
-        return {
-            statusCode: 200,
-            body: JSON.stringify({ message: answer }),
-        }
-    } catch (error) {
-        return { statusCode: 500, body: error.toString() }
-    } 
+  const dotProduct = a.reduce((acc, val, i) => acc + val * b[i], 0);
+  const normA = Math.sqrt(a.reduce((acc, val) => acc + val * val, 0));
+  const normB = Math.sqrt(b.reduce((acc, val) => acc + val * val, 0));
+  return dotProduct / (normA * normB);
 }
 
+async function handler(event) {
+  try {
+    const embeddings = await getEmbeddings();
+
+    const userQuestion = event.queryStringParameters.question || 'What is Pachw?'
+    console.log("subject", userQuestion)
+
+    const similarities = embeddings.map((embedding) => {
+      const article = embedding.text;
+      const embeddingVector = embedding.embeddings.split(',').map(parseFloat);
+      const userQuestionVector = userQuestion.split(' ').map((word) => embeddingVector[embedding.text.indexOf(word)] || 0);
+      const similarity = cosineSimilarity(embeddingVector, userQuestionVector);
+      return { article, similarity };
+    });
+
+    // Sort the similarities in descending order by the similarity score
+    similarities.sort((a, b) => b.similarity - a.similarity);
+
+    const prompt = similarities[0].article.substring(0, 1500);
+
+    const request = new CreateAnswerRequest({
+      model: "text-davinci-002",
+      prompt: prompt,
+      question: userQuestion,
+      maxTokens: 200,
+      nExamples: 1,
+      examplesContext: prompt,
+      examples: [
+        ["What is Pachyderm?","Pachyderm is a data science platform."],
+        ["What is data versioning?","Data versioning is the practice of keeping track of changes to data over time."],
+      ],
+      stop: ["\n"],
+    });
+
+    const response = await openai.createAnswer(request);
+
+    console.log("response", response)
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: response.data.answers[0] }),
+    }
+  } catch (error) {
+    return { statusCode: 500, body: error.toString() }
+  } 
+}
 
 module.exports = { handler }
