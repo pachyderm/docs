df9a09f2104e96db27fb14f63f8bcad50b82adeb
L.B <mrlawrencelane@gmail.com>
2023-03-16 11:00:46 -0400

    undo

--- netlify/functions/ask/ask.js
+++ netlify/functions/ask/ask.js
@@ -8,60 +8,63 @@ const Papa = require("papaparse");
 const axios = require("axios");
 
 async function getEmbeddings() {
-  const response = await axios.get("https://deploy-preview-39--pach-docs.netlify.app/embeddings/embeddings.csv");
-  const embeddings = Papa.parse(response.data, {
-    header: true,
-    dynamicTyping: true,
-    skipEmptyLines: true,
-  }).data;
-  return embeddings;
-}
+    const response = await axios.get("https://deploy-preview-39--pach-docs.netlify.app/embeddings/embeddings.csv");
+    const embeddings = Papa.parse(response.data, {
+      header: true,
+      dynamicTyping: true,
+      skipEmptyLines: true,
+    }).data;
+    return embeddings;
+  }
+  
 
 // function to calculate cosine similarity between two vectors
 function cosineSimilarity(a, b) {
-  const dotProduct = a.reduce((acc, val, i) => acc + val * b[i], 0);
-  const normA = Math.sqrt(a.reduce((acc, val) => acc + val * val, 0));
-  const normB = Math.sqrt(b.reduce((acc, val) => acc + val * val, 0));
-  return dotProduct / (normA * normB);
-}
+    const dotProduct = a.reduce((acc, val, i) => acc + val * b[i], 0);
+    const normA = Math.sqrt(a.reduce((acc, val) => acc + val * val, 0));
+    const normB = Math.sqrt(b.reduce((acc, val) => acc + val * val, 0));
+    return dotProduct / (normA * normB);
+  }
 
 async function handler(event) {
-  try {
-    const embeddings = await getEmbeddings();
+    try {
+        const embeddings = await getEmbeddings();
 
-    const userQuestion = event.queryStringParameters.question || 'What is Pachw?'
-    console.log("subject", userQuestion)
+        const userQuestion = event.queryStringParameters.question || 'What is Pachw?'
+        console.log("subject", userQuestion)
 
-    const similarities = embeddings.map((embedding) => {
-      const article = embedding.text;
-      const embeddingVector = embedding.embeddings.split(',').map(parseFloat);
-      const userQuestionVector = userQuestion.split(' ').map((word) => embeddingVector[embedding.text.indexOf(word)] || 0);
-      const similarity = cosineSimilarity(embeddingVector, userQuestionVector);
-      return { article, similarity };
-    });
+        const similarities = embeddings.map((embedding) => {
+            const article = embedding.text;
+            const embeddingVector = embedding.embeddings.split(',').map(parseFloat);
+            const userQuestionVector = userQuestion.split(' ').map((word) => embeddingVector[embedding.text.indexOf(word)] || 0);
+            const similarity = cosineSimilarity(embeddingVector, userQuestionVector);
+            return { article, similarity };
+          });
+        
+        // Sort the similarities in descending order by the similarity score
 
-    // Sort the similarities in descending order by the similarity score
-    similarities.sort((a, b) => b.similarity - a.similarity);
+        similarities.sort((a, b) => b.similarity - a.similarity);
+        
+        const maxLength = 1500;
+        const prompt = `${userQuestion}\n${similarities[0].article.substring(0, maxLength)}`;
+        console.log("prompt", prompt)
 
-    const prompt = `${userQuestion}\n${similarities[0].article.substring(0, 1500)}`;
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: prompt,
+            temperature: 0,
+            max_tokens: 200,
+        });
 
-    const response = await openai.createCompletion({
-      model: "text-davinci-002",
-      prompt: prompt,
-      temperature: 0,
-      maxTokens: 200,
-      stop: ["\n"],
-    });
+        console.log("response", response)
 
-    console.log("response", response)
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: response.data.choices[0].text }),
-    }
-  } catch (error) {
-    return { statusCode: 500, body: error.toString() }
-  } 
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: response.data.choices[0].text  }),
+        }
+    } catch (error) {
+        return { statusCode: 500, body: error.toString() }
+    } 
 }
 
 module.exports = { handler }
