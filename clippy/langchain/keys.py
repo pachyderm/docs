c4ee4c3e3f156d23e84f4f16eee063c67ad21eba
L.B <mrlawrencelane@gmail.com>
2023-05-17 14:55:30 -0400

    keys

--- clippy/langchain/keys.py
+++ clippy/langchain/keys.py
@@ -1,5 +1,5 @@
 import os
 
 openai_key = os.environ.get('OPENAI_API_KEY')
-pinecone_key = os.environ.get('PINECONE_API_KEY') or "be596285-2253-4a51-b363-13ffd1cc589f"
-pinecone_environment = os.environ.get('PINECONE_ENVIRONMENT') or "us-central1-gcp"
+pinecone_key = os.environ.get('PINECONE_API_KEY') 
+pinecone_environment = os.environ.get('PINECONE_ENVIRONMENT') 
