1085311abdf03ee6b9ccdbb601ffea8c5df733c0
L.B <mrlawrencelane@gmail.com>
2023-05-22 14:45:59 -0400

    removed other versions

--- clippy/fastAPI/request.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = "http://localhost:8000/answer"
-
-question = "What is Pachyderm?"
-
-response = requests.post(url, json={"question": question})
-
-print(response.json())
--- clippy/langchain/config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-openai_key = os.environ.get('OPENAI_API_KEY')
-pinecone_key = os.environ.get('PINECONE_API_KEY')
-pinecone_environment = os.environ.get('PINECONE_ENVIRONMENT')
-pinecone_index = "langchain1"
