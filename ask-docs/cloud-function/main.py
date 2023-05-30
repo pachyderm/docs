391103dac62a0f3addfd685ec04e35329f76c72b
L.B <mrlawrencelane@gmail.com>
2023-05-30 09:40:41 -0400

    clippy function update

--- ask-docs/cloud-function/main.py
+++ ask-docs/cloud-function/main.py
@@ -1,4 +1,5 @@
 import os
+import functions_framework
 
 from langchain.llms import OpenAI 
 from langchain.chains.question_answering import load_qa_chain
@@ -32,7 +33,7 @@ def answer_question(question: str, vs, chain, memory):
     memory.save_context(inputs={"question": question}, outputs={"answer": answer})
     return {"answer": answer}
 
-llm = OpenAI(temperature=0, openai_api_key=openai_key, max_tokens=-1) 
+llm = OpenAI(temperature=1, openai_api_key=openai_key, max_tokens=-1, streaming=True) 
 chain = load_qa_chain(llm, chain_type="stuff")
 embeddings = OpenAIEmbeddings(openai_api_key=openai_key)
 docsearch = Pinecone.from_existing_index(pinecone_index, embeddings)
@@ -42,6 +43,27 @@ import functions_framework
 
 @functions_framework.http
 def start(request):
+    # For more information about CORS and CORS preflight requests, see:
+    # https://developer.mozilla.org/en-US/docs/Glossary/Preflight_request
+
+    # Set CORS headers for the preflight request
+    if request.method == 'OPTIONS':
+        # Allows GET requests from any origin with the Content-Type
+        # header and caches preflight response for an 3600s
+        headers = {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Max-Age': '3600'
+        }
+
+        return ('', 204, headers)
+
+    # Set CORS headers for the main request
+    headers = {
+        'Access-Control-Allow-Origin': '*'
+    }
+
     request_json = request.get_json(silent=True)
     request_args = request.args
 
@@ -54,4 +76,4 @@ def start(request):
         question = 'What is Pachyderm?'
 
 
-    return answer_question(question=question, vs=docsearch, chain=chain, memory=memory)
+    return (answer_question(question=question, vs=docsearch, chain=chain, memory=memory), 200, headers)
