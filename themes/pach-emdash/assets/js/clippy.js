7068f23b4945000098963f5a066ba19a6552fd04
L.B <mrlawrencelane@gmail.com>
2023-05-31 09:04:57 -0400

    padding

--- themes/pach-emdash/assets/js/clippy.js
+++ themes/pach-emdash/assets/js/clippy.js
@@ -6,6 +6,8 @@ document.addEventListener('DOMContentLoaded', () => {
   scrollToLastQuestion();
 });
 
+// Input 
+
 async function submitQuestion(event) {
   event.preventDefault();
 
@@ -17,7 +19,6 @@ async function submitQuestion(event) {
     const data = await fetchAnswerFromFunction(question);
     qaSetContainer.classList.remove('pulse');
     
-
     const answerContainer = createAnswerContainer(data.error || `<strong>A:</strong> ${data.answer}`);
     const docsContainer = createDocsContainer(data.docs);
     
@@ -31,68 +32,17 @@ async function submitQuestion(event) {
   }
 }
 
-function appendElementsToContainer(container, elements) {
-  elements.forEach(element => container.appendChild(element));
+function clearQuestionInput() {
+  document.getElementById('question').value = '';
 }
 
+// API, Data
 async function fetchAnswerFromFunction(question) {
   const response = await fetch(`https://pach-docs-chatgpt-6ukzwpb5kq-uc.a.run.app/?query=${encodeURIComponent(question)}`);
   return await response.json();
 }
 
-function createAnswerContainer(html) {
-  const answer = document.createElement('div');
-  answer.classList.add('py-2');
-  answer.innerHTML = html;
-  return answer;
-}
-
-function createDocsContainer(docs) {
-  const docsContainer = document.createElement('div');
-  docsContainer.classList.add('spread-left', 'm-1', 'py-1', 'c-sp-2', 'rounded-2');
-
-  docs.forEach(doc => {
-    const docLink = document.createElement('a');
-    docLink.classList.add('black', 'rounded-2', 'is-fit', 'xs', 'outlined');
-    docLink.href = doc.relURI;
-    docLink.target = '_blank';
-    docLink.innerText = doc.title;
-    docsContainer.appendChild(docLink);
-  });
-
-  return docsContainer;
-}
-
-function createRemoveButton(id) {
-  const removeButton = document.createElement('button');
-  removeButton.classList.add('remove', 'sp-1', 'rounded-2');
-  removeButton.innerHTML = '🗑️';
-  removeButton.addEventListener('click', () => clearQASet(id));
-  return removeButton;
-}
-
-function createQASetContainer(question) {
-  
-  const qaSetContainer = document.createElement('div');
-  qaSetContainer.id = Date.now().toString();
-  qaSetContainer.classList.add('darken-1', 'p-1', 'rounded-2')
-
-  const questionContainer = document.createElement('div');
-  questionContainer.classList.add('py-2')
-  questionContainer.innerHTML = `<strong>Q:</strong> ${question}`;
-  qaSetContainer.appendChild(questionContainer);
-
-  chatTrayContainer.appendChild(qaSetContainer);
-  clearQuestionInput();
-  scrollToLastQuestion();
-
-  return qaSetContainer;
-}
-
-function clearQuestionInput() {
-  document.getElementById('question').value = '';
-}
-
+// Conversation (Local Storage)
 function storeConversation() {
   localStorage.setItem('conversation', JSON.stringify(conversation));
   console.log(conversation);
@@ -127,10 +77,72 @@ function clearQASet(id) {
   storeConversation();
 }
 
+// UI
+
+function appendElementsToContainer(container, elements) {
+  elements.forEach(element => container.appendChild(element));
+}
+
+function createAnswerContainer(html) {
+  const answer = document.createElement('div');
+  answer.classList.add('py-2');
+  answer.innerHTML = html;
+  return answer;
+}
+
+function createDocsContainer(docs) {
+  const docsContainer = document.createElement('div');
+  docsContainer.classList.add('spread-left', 'py-1', 'c-p-2', 'c-mr-2', 'c-my-2', 'rounded-2');
+
+  const uniqueDocs = docs.filter((doc, index, self) =>
+    index === self.findIndex((d) => (
+      d.title === doc.title
+    ))
+  );
+
+  uniqueDocs.forEach(doc => {
+    const docLink = document.createElement('a');
+    docLink.classList.add('black', 'rounded-2', 'is-fit', 'xs', 'outlined');
+    docLink.href = doc.relURI;
+    docLink.target = '_blank';
+    docLink.innerText = doc.title;
+    docsContainer.appendChild(docLink);
+  });
+
+  return docsContainer;
+}
+
+function createQASetContainer(question) {
+  
+  const qaSetContainer = document.createElement('div');
+  qaSetContainer.id = Date.now().toString();
+  qaSetContainer.classList.add('darken-1', 'px-3', 'rounded-2', 'outlined-white')
+
+  const questionContainer = document.createElement('div');
+  questionContainer.classList.add('py-2')
+  questionContainer.innerHTML = `<strong>Q:</strong> ${question}`;
+  qaSetContainer.appendChild(questionContainer);
+
+  chatTrayContainer.appendChild(qaSetContainer);
+  clearQuestionInput();
+  scrollToLastQuestion();
+
+  return qaSetContainer;
+}
+
+function createRemoveButton(id) {
+  const removeButton = document.createElement('button');
+  removeButton.classList.add('remove', 'sp-1', 'rounded-2');
+  removeButton.innerHTML = '🗑️';
+  removeButton.addEventListener('click', () => clearQASet(id));
+  return removeButton;
+}
+
 function scrollToLastQuestion() {
   chatTrayContainer.scrollTop = chatTrayContainer.scrollHeight;
 }
 
+// Navigation
 function openDocument(relURI) {
   window.open(relURI, '_blank');
 }
