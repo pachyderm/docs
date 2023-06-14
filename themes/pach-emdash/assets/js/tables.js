e2513b0621f90d72833128b4728884ee7a7b82f1
L.B <mrlawrencelane@gmail.com>
2023-06-14 11:23:40 -0400

    styling fixes

--- themes/pach-emdash/assets/js/tables.js
+++ themes/pach-emdash/assets/js/tables.js
@@ -5,10 +5,12 @@ var tables = document.querySelectorAll('table');
 // wrap the table in a div with a class of 'table-wrapper'
 
 tables.forEach(function(table) {
-    var wrapper = document.createElement('div');
-    wrapper.classList.add('tableWrapper');
+    var wrapper1 = document.createElement('div');
+    var wrapper2 = document.createElement('div');
+    wrapper2.classList.add('tableWrapper');
 
-    table.parentNode.insertBefore(wrapper, table);
-    wrapper.appendChild(table);
+    table.parentNode.insertBefore(wrapper1, table);
+    wrapper1.appendChild(wrapper2);
+    wrapper2.appendChild(table);
     } 
 );
